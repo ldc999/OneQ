c1fb7a6b84459752bd89314a852ca09d4e2b3063
ldc999 <ldc999@live.com>
2019-07-24 23:58:29 +0800

    引用colorUI CSS样式

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ App({
   },
   onLaunch: function () {
     try {
-      wx.clearStorageSync()
+     // wx.clearStorageSync();
+      wx.getSystemInfo({
+        success: e => {
+          this.globalData.StatusBar = e.statusBarHeight;
+          let custom = wx.getMenuButtonBoundingClientRect();
+          this.globalData.Custom = custom;
+          this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+        }
+      })
     } catch (e) { }
   },
   getUserInfo: function (cb) {
@@ -20,67 +28,11 @@ App({
       wx.login({ 
         success: res => {
           // 发送 res.code 到后台换取 openId, sessionKey, unionId
-          if (res.code) {
-            
+          if (res.code) { 
             wx.getUserInfo({
               success: function (resS) {
-                that.globalData.userInfo = resS.userInfo
-                //1.先获取openid
-                wx.request({
-                  url: 'http://129.204.166.225/SteelService.asmx/GetOpenID',
-                  method: 'post',
-                  data: {
-                    code: res.code
-                  },
-                  header: {
-                    'content-type': 'application/x-www-form-urlencoded'
-                  },
-                  success: function (res) {
-                    var response = res.data;
-                    console.log("res:" + res.data);
-                    that.globalData.openid = res.data.openid;//获取到的openid
-                    console.log(that.globalData.openid);
-                    //2.获取opnenid成功后登陆(注册)获取userId
-                    typeof cb == "function" && cb(that.globalData.userInfo)
-                    var usermodel = {};
-                    usermodel.Name = resS.userInfo.nickName;
-                    usermodel.Phone = '18712345678';
-                    usermodel.WechatCode = that.globalData.openid;
-                    usermodel.status = '1';
-                    console.log(JSON.stringify(usermodel))
-                    wx.request({
-                      url: 'http://129.204.166.225/SteelService.asmx/LoginUserBackID',
-                      method: 'post',
-                      data: {
-                        model: JSON.stringify(usermodel)
-                      },
-                      header: {
-                        'content-type': 'application/x-www-form-urlencoded'
-                      },
-                      success: function (res) {
-                        var response = res.data;
-                        console.log(res);
-                        if (response > 0) {
-                          //保存为全局变量:
-                          console.log('LoginUserBackID:' + response)
-                        }
-                        else //如果找不到，添加该用户也失败了
-                        {
-                          console.log('response' + response)
-                        }
-                      },
-                      fail: function () {
-                        console.log('FAIL')
-                        return null
-                      }
-                    })
-                  },
-                  fail: function () {
-                    console.log('FAIL')
-                    return null
-                  }
-                })
-               
+                that.globalData.userInfo = resS.userInfo ;
+              typeof cb == "function" && cb(that.globalData.userInfo)  
               }
             })
           }
--- pages/person/person.js
+++ pages/person/person.js
@@ -1,12 +1,15 @@
 var app = getApp()
-Page({
+Component({
+  options: {
+    addGlobalClass: true,
+  },
   data: {
     userInfo: {},
     myProfile: [{ "desc": "我的等级", "id": "coin" }, { "desc": "我问的", "id": "myQues" }, { "desc": "我回答的", "id": "myHeared" }],
     myAccount: ["手机号码", "帮助", "结算说明", "关于一问十答"]
   },
-  onLoad: function () {
-    console.log('onLoad')
+  attached() {
+    console.log("success")
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function (userInfo) {
@@ -15,11 +18,5 @@ Page({
         userInfo: userInfo
       })
     })
-  },
-  onShow: function () {
-    console.info("show")
-  },
-  loadProfile: function (e) {
-    console.log(e.target)
   }
-})
+  }) 
